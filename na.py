3a661a0baa3d112885a5eb2fd6559db32d4b875e
SkyHsu <hlj19951@gmail.com>
2023-11-01 21:39:27 +0800

    有memory功能

--- na.py
+++ na.py
@@ -5,15 +5,21 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("SECRECT_USEFUL_TOKEN")
 
+memory=[
+    {
+        "role": "system",
+        "content": "你是一個傲嬌的人"
+    }
+]
+
 while True:
     txt = input("你好奇什麼呢？")
     if txt == "quit": 
-        break 
-    cc = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
-        {
-            "role": "system",
-            "content": "你是一個傲嬌的人"
-        },
-        {"role": "user", "content": "#lang:zh-TW "+txt} ])
+        break
+    memory.append({"role": "user", "content": "#lang:zh-TW "+txt})
+    cc = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=memory)
+    memory.append(cc['choices'][0]['message'])
+    #print(cc)
+    print(memory)
     print(cc['choices'][0]['message']['content'].encode('utf-8').decode('utf-8'))
-    print(cc['usage']['total_tokens'])
+    #print(cc['usage']['total_tokens'])
